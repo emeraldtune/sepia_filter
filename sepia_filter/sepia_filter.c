c25ce3c04e2ddbf319897b5e38e2fc2b2a9e7e58
emeraldtune <emeraldtune@yandex.ru>
2015-01-09 20:24:21 +0300

    Opens the file

--- sepia_filter/sepia_filter.c
+++ sepia_filter/sepia_filter.c
@@ -1,9 +1,24 @@
-
+#include <stdio.h>
+#include <conio.h>
 
 
 
 int main(int argc, char* argv[])
 {
+	FILE *in;
+
+	fopen_s(&in,"pict.jpg","rb");//открываем указанный в аргументе файл для чтения в бинарном режиме 
+
+	if (in==NULL) 
+	{
+		printf("Can not open the file");
+		_getch();
+		return 1; //выход
+	}
+
+
+	fclose(in);
+	_getch();
 	return 0;
 }
 
